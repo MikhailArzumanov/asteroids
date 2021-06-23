a53d05597a8265f88a6420f1cf96f10a4cc6b0bb
Mikhaill <mikhailArzumanov@yandex.ru>
2021-06-23 23:51:44 +0300

    Asteroid gen refactored

--- Asteroid.cpp
+++ Asteroid.cpp
@@ -2,12 +2,16 @@
 #include "environment.hpp"
 #include <iostream>
 
-float gen_rand_coord(const int DIM, int r) {
-	int res = rand() % (DIM + 4*r) - 2*r;
-	if (0 <= res  && res <= SCREEN_WIDTH) {
-		if (res < DIM/2)
+float gen_rand_X(int r) {
+	return (rand()%(SCREEN_WIDTH+4*r)) - 2*r;
+}
+
+float gen_rand_Y(int r) {
+	int res = rand() % (SCREEN_HEIGHT + 4*r) - 2*r;
+	if (0 <= res  && res <= SCREEN_HEIGHT) {
+		if (res < SCREEN_HEIGHT /2)
 			 res = -2*r;
-		else res = DIM+2*r;
+		else res = SCREEN_HEIGHT +2*r;
 	}
 	return static_cast<float>(res);
 }
@@ -16,8 +20,8 @@ Asteroid::Asteroid(float speed_coef) {
 	type = asteroid_type;
 	lifetime = -1; r = 50;
 	sprite = &(sprites[astroid]);
-	float start_X = rand() % (SCREEN_WIDTH + static_cast<int>(4*r)) - static_cast<int>(2*r);
-	float start_Y = gen_rand_coord(SCREEN_HEIGHT, static_cast<int>(r));
+	float start_X = gen_rand_X(static_cast<int>(r));
+	float start_Y = gen_rand_Y(static_cast<int>(r));
 	p = { start_X, start_Y };
 	point spacecraft_point = spacecraft->get_point();
 	v = (spacecraft_point - p)*speed_coef;
