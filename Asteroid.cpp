d5676a83bb2b975387851f6c8a624cbad9c4dc42
Mikhaill <mikhailArzumanov@yandex.ru>
2021-06-23 23:55:26 +0300

    Asteroid gen refactored

--- Asteroid.cpp
+++ Asteroid.cpp
@@ -2,12 +2,18 @@
 #include "environment.hpp"
 #include <iostream>
 
+int get_rand(int from, int to) {
+	int res = (rand() % (to-from)) + from;
+	return res;
+}
+
 float gen_rand_X(int r) {
-	return (rand()%(SCREEN_WIDTH+4*r)) - 2*r;
+	int res = get_rand(-2*r,SCREEN_WIDTH+2*r);
+	return static_cast<float>(res);
 }
 
 float gen_rand_Y(int r) {
-	int res = rand() % (SCREEN_HEIGHT + 4*r) - 2*r;
+	int res = get_rand(-2*r,SCREEN_HEIGHT+2*r);
 	if (0 <= res  && res <= SCREEN_HEIGHT) {
 		if (res < SCREEN_HEIGHT /2)
 			 res = -2*r;
