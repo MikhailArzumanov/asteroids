66b273e9c285057aa6b5116c41388b47f7686b3f
Арзуманов Михаил <marzumanov@softrust.ru>
2022-01-25 16:20:17 +0300

    debug content was placed at main file

--- Asteroids.cpp
+++ Asteroids.cpp
@@ -1,6 +1,16 @@
-﻿
+﻿#include <SFML/Graphics.hpp>
+#include "window.hpp"
 
-int main(){
-
+int main() {
+	sf::Texture texture; texture.loadFromFile("textures/spacecraft.png");
+	sf::Sprite sprite(texture);
+	float angle = 0.f;
+	window = new sf::RenderWindow(sf::VideoMode(1280, 720), "Window");
+	while (window->isOpen()) {
+		sprite.setRotation(angle);
+		window->draw(sprite);
+		angle += 0.01f;
+		window->display();
+	}
 }
 
