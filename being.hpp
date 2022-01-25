26f6846f97c04d4037c1fe22b394cdfb1f0043b0
Арзуманов Михаил <marzumanov@softrust.ru>
2022-01-25 16:25:38 +0300

    being class was modified

--- being.hpp
+++ being.hpp
@@ -2,8 +2,11 @@
 #include "circle.hpp"
 #include "ILiving.hpp"
 #include "IDrawable.hpp"
-
+#include <SFML/Graphics/Sprite.hpp>
 class Being : public Circle, public ILiving, public IDrawable {
+protected:
+	float angle;
+	sf::Sprite* sprite;
 public:
 	virtual bool is_living();
 };
