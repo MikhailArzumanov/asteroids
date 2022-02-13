c5d9c9415275d3833fd4a2f547d658a6329f811c
Арзуманов Михаил <marzumanov@softrust.ru>
2022-02-13 15:00:22 +0300

    circle's collide with another one was relayed to private method

--- circle.hpp
+++ circle.hpp
@@ -2,6 +2,8 @@
 #include "shape.hpp"
 
 class Circle : public Shape {
+private:
+	bool is_collides_with_circle(Shape* another);
 protected:
 	float r;
 public:
--- circle_is_collides_with.cpp
+++ circle_is_collides_with.cpp
@@ -1,14 +1,16 @@
 #include "circle.hpp"
 
+bool Circle::is_collides_with_circle(Shape* another) {
+	Circle* circle_ = static_cast<Circle*>(another);
+	float r_sum = r + circle_->r;
+	float dist = p.get_sqr_dist(another->p);
+	return dist <= r_sum * r_sum;
+}
+
 bool Circle::is_collides_with(Shape* another) {
-	Circle* circle_;
-	float r_sum,dist;
 	switch(another->type){
 	case circle:
-		circle_ = static_cast<Circle*>(another);
-		r_sum	= r + circle_->r;
-		dist	= p.get_sqr_dist(another->p);
-		return dist <= r_sum*r_sum;
+		return is_collides_with_circle(another);
 	default:
 		return false;
 	}
