e813efd6c6e44e6cd590ae4274c050e477234c6b
Арзуманов Михаил <marzumanov@softrust.ru>
2022-02-13 16:33:15 +0300

    debug data was added

--- Asteroids.cpp
+++ Asteroids.cpp
@@ -1,15 +1,18 @@
 ﻿#include <SFML/Graphics.hpp>
 #include "window.hpp"
+#include "point.hpp"
+#include "spacecraft.hpp"
 
 int main() {
 	sf::Texture texture; texture.loadFromFile("textures/spacecraft.png");
 	sf::Sprite sprite(texture);
+	sprite.setPosition(200, 200);
 	float angle = 0.f;
 	window = new sf::RenderWindow(sf::VideoMode(1280, 720), "Window");
+	auto spacecraft = Spacecraft(&sprite);
 	while (window->isOpen()) {
-		sprite.setRotation(angle);
-		window->draw(sprite);
-		angle += 0.01f;
+		spacecraft.draw();
+		spacecraft.tick();
 		window->display();
 	}
 }
